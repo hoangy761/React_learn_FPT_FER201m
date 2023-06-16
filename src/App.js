ba928af89ebc69baaa4aabb5a40eb521487b4d74
Yizy <Ybhde160208@fpt.edu.vn>
2023-06-16 15:08:28 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
+import Menu from './components/MenuComponent';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Component from 'react'
+import { Component } from 'react';
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
