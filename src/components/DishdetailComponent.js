a005cf9ad52d2ba5a7ab2e86616d80668814d89a
Yizy <Ybhde160208@fpt.edu.vn>
2023-06-16 21:36:38 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardBody,
-    CardTitle, CardImgOverlay } from 'reactstrap';
-
+    import { Card, CardImg, CardText, CardBody,CardImgOverlay,
+        CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    import { Link } from 'react-router-dom';
+    
 
 
     function RenderDish({dish}) {
@@ -71,11 +72,29 @@ const  DishDetail = (props) => {
     const dishItem = RenderDish(dish);
     const commentItem = RenderComments(dish.comments);
     return (
-        <div className='col-12 col-md-5 m-1'>
-            {dishItem}
-            {commentItem}
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
         </div>
-    )
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+        </div>
+    );
+
 }
 
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -28,19 +28,17 @@ class Main extends Component {
 
 
   onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
+    this.setState({ selectedDish: dishId });
   }
 
   render() {
-    const HomePage = () => {
-      return(
-          <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
       );
-    }
+    };
+
 
 
     return (
@@ -48,20 +46,20 @@ class Main extends Component {
         <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        
+
         <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
-              <Route exact path='/contactus' component={Contact} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+          <Route exact path='/contactus' component={Contact} />
         </Switch>
         <Footer />
-        
+
       </div>
     );
-    
+
   }
-  
+
 }
 
 
