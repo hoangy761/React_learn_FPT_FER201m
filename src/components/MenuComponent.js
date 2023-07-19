a2539054283a6b7e2ddd39fc0724e5dd3ba980b5
Yizy <Ybhde160208@fpt.edu.vn>
2023-07-19 09:21:34 +0700

    Fetch Post Comment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -27,6 +27,7 @@ const Menu = (props) => {
             <div className="col-12 col-md-5 m-1" key={dish.id}>
                 <RenderMenuItem dish={dish} onClick={props.onClick} />
                 <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
     });
