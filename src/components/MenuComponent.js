38d53ef83459c2e08304370daf36ebcf6ad12ffc
Yizy <Ybhde160208@fpt.edu.vn>
2023-07-19 09:18:32 +0700

    Redux Actions

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -11,8 +11,8 @@ function RenderMenuItem({ dish, onClick }) {
         <Card>
             <Link to={`/menu/${dish.id}`} >
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
+                    <CardTitle>{dish.name} .</CardTitle>
                     <CardTitle>{dish.name} .</CardTitle>
                 </CardImgOverlay>
             </Link>
